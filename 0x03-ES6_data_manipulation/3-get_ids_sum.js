dfd1b9c026d66daca86deb1548fc60b7227fe4ce
Thermiprettie <tadekolasarah@yahoo.com>
2023-07-03 20:37:28 +0000

    0x03. ES6 data manipulation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,5 @@
-const getStudentIdsSum = (students) => students.reduce((st8, sval) => st8 + sval, 0);
+function getStudentIdsSum(students) {
+	return students.reduce((st8, sval) => st8 + sval, 0);
+}
 
 export default getStudentIdsSum;
